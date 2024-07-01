d29735f22422d7439b071bbbed9ee1e71d91ca8d
RAFI <gollarafi@gmail.com>
2024-07-01 09:11:39 +0800

    basic setup done

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <p className="text-7xl"> Hi i am navbar</p>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar/Navbar";
+
+const Main = () => {
+    return (
+        <div className="max-w-screen-2xl bg-[#151515] scroll-smooth">
+            <Navbar />
+        </div>
+    );
+};
+
+export default Main;
