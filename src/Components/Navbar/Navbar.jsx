0045045d4b211aa737385e9329577bcfc1cc5b93
RAFI <gollarafi@gmail.com>
2024-07-01 10:18:25 +0800

    navbar done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
+import githubIcon from "../../assets/github.svg"
+import xIcon from "../../assets/X.svg"
+import linkedInIcon from "../../assets/linkedin.svg"
+
 
 const Navbar = () => {
     return (
-        <div>
-            <p className="text-7xl"> Hi i am navbar</p>
+        <div className="flex justify-between pt-10">
+            <div><p className="text-[32px] font-bold">R A F I</p></div>
+            <div className="flex gap-[30px] items-center">
+                <img className="" src={githubIcon} alt="github icon" />
+                <img className="h-[24px] w-[24px]" src={xIcon} alt="x icon" />
+                <img className="" src={linkedInIcon} alt="linkedIn icon" />
+            </div>
         </div>
     );
 };
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,7 +2,7 @@ import Navbar from "../Components/Navbar/Navbar";
 
 const Main = () => {
     return (
-        <div className="max-w-screen-2xl bg-[#151515] scroll-smooth">
+        <div className="max-w-screen-2xl mx-auto bg-[#151515] scroll-smooth px-[165px]">
             <Navbar />
         </div>
     );
