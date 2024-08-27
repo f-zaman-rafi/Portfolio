ea367a5e3b8e5568ebb20e48ee31d0a38f1a6f83
RAFI <gollarafi@gmail.com>
2024-08-27 18:52:06 +0800

    fix some texts

--- src/Components/Qualifications/Education.jsx
+++ src/Components/Qualifications/Education.jsx
@@ -8,18 +8,20 @@ const Education = () => {
             <h1 className="text-[40px] md:text-7xl lg:text-[88px] font-bold text-white">Education</h1>
             <div className="pt-10 md:pt-16 lg:pt-20">
                 <div className="flex flex-col md:flex-row items-start md:items-center justify-start md:justify-center md:gap-20 lg:gap-32 lg:hover:gap-20 group hover:text-white" style={{ transition: '900ms' }}>
-                    <div><h1 className="text-3xl md:text-5xl lg:text-7xl py-5  group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}>2024</h1></div>
-                    <div className="md:border-b-2 max-w-3xl pb-10 group-hover:border-[#4EE1A0]" style={{ transition: '900ms' }}>
-                        <p className="hover:text-white  text-gray-700 " style={{ transition: '1000ms' }}><span className="font-bold text-2xl group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}> 2024 | January - June </span> Successfully completed the "Complete Web Development Course" by Programming Hero, where I developed a strong foundation in the MERN stack, including ES6, Restful APIs, JWT Authentication,npm packages, GitHub, Chrome Dev Tools, Firebase, and Netlify. This course equipped me with modern web development skills and best practices, leading to a certification that validates my expertise. </p>
+                    <div className="w-52 lg:text-center"><h1 className="text-3xl md:text-5xl lg:text-7xl py-5  group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}>2023</h1></div>
+                    <div className="md:border-b-2 max-w-3xl md:pb-10  group-hover:border-[#4EE1A0]" style={{ transition: '900ms' }}>
+                        <p className="hover:text-white text-gray-700 " style={{ transition: '1000ms' }} ><span className="font-bold text-2xl group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}> 2023 - 2026 </span>I'm currently pursuing a Bachelor's in Computer Science at Lincoln University College, where I’m excelling in core concepts and actively applying theoretical knowledge to real-world challenges. My academic journey is driven by a strong commitment to learning, problem-solving, and continuous improvement.</p>
                     </div>
                 </div>
 
                 <div className="flex flex-col md:flex-row items-start md:items-center justify-start md:justify-center md:gap-20 lg:gap-32 lg:hover:gap-20 group hover:text-white" style={{ transition: '900ms' }}>
-                    <div><h1 className="text-3xl md:text-5xl lg:text-7xl py-5  group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}>2023</h1></div>
-                    <div className="md:border-t-2 max-w-3xl md:pt-10  group-hover:border-[#4EE1A0]" style={{ transition: '900ms' }}>
-                        <p className="hover:text-white text-gray-700 " style={{ transition: '1000ms' }} ><span className="font-bold text-2xl group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}> 2023 -2026 </span>Currently enrolled in the Bachelor’s program for Computer Science at Lincoln University College, where I am excelling in my studies and developing a deep understanding of core computer science concepts. My academic journey is marked by a commitment to learning, problem-solving, and applying theoretical knowledge to practical scenarios.</p>
+                    <div className="w-52 lg:text-center"><h1 className="text-3xl md:text-5xl lg:text-7xl  pb-5 pt-10 group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}>2017</h1></div>
+                    <div className="md:border-t-2 max-w-3xl md:pt-10 group-hover:border-[#4EE1A0]" style={{ transition: '900ms' }}>
+                        <p className="hover:text-white text-gray-700 " style={{ transition: '1000ms' }}><span className="font-bold text-2xl group-hover:text-[#4EE1A0]" style={{ transition: '900ms' }}> 2017 - 2022 </span>
+                            I earned my diploma in Computer Technology from Narshingdi Polytechnic Institute in 2022, where I developed a strong foundation in software development, networking, and hardware systems. This hands-on experience has equipped me with the technical skills and adaptability needed to excel in the rapidly evolving tech industry.</p>
                     </div>
                 </div>
+
             </div>
         </div >
         </Element >
